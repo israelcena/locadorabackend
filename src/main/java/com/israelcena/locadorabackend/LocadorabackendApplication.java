82f71cace0d1d2994b1d238494cfdb6c46dc0dfe
Israel Cena <israelsjm@gmail.com>
2022-04-02 15:37:13 -0300

    Initial commit

---
+++ src/main/java/com/israelcena/locadorabackend/LocadorabackendApplication.java
@@ -0,0 +1,13 @@
+package com.israelcena.locadorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadorabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadorabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelcena/locadorabackend/LocadorabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelcena.locadorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadorabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
