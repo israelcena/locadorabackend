d7f9b9d10ac81e188ae05b82fe5215a7c4be382d
Israel Cena <israelsjm@gmail.com>
2022-04-02 17:12:47 -0300

    create REST HealthCheck

---
+++ src/main/java/com/israelcena/locadorabackend/cliente/controller/HealthCheck.java
@@ -0,0 +1,17 @@
+package com.israelcena.locadorabackend.cliente.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/healthcheck")
+@RestController
+public class HealthCheck {
+
+  @GetMapping
+  public String healthCheck(){
+    return "This Server is Alive !";
+  }
+
+  @PostMapping @ResponseStatus( HttpStatus.OK )
+  public void postCheck(){}
+}
