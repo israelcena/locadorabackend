0c7f40fd40f659b336c933fbfef0e44df3d8ee67
Israel Cena <israelsjm@gmail.com>
2022-04-02 19:07:56 -0300

    change HealthCheck endpoint

--- src/main/java/com/israelcena/locadorabackend/cliente/controller/HealthCheck.java
+++ src/main/java/com/israelcena/locadorabackend/cliente/controller/HealthCheck.java
@@ -3,7 +3,7 @@ package com.israelcena.locadorabackend.cliente.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/healthcheck")
+@RequestMapping("/cliente/healthcheck")
 @RestController
 public class HealthCheck {
 
