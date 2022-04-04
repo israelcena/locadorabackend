75ea436d4b9ce4915fed223e8c90ccf6a12ecc31
Israel Cena <israelsjm@gmail.com>
2022-04-04 07:40:30 -0300

    include fimesdto objects

--- src/main/java/com/israelcena/locadorabackend/filmes/dto/FilmesDTO.java
+++ src/main/java/com/israelcena/locadorabackend/filmes/dto/FilmesDTO.java
@@ -1,4 +1,17 @@
 package com.israelcena.locadorabackend.filmes.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class FilmesDTO {
+
+  private String titulo;
+  private String descricao;
+  private String genero;
+  private Float nota;
+  private Boolean disponibilidade = true;
+
 }
