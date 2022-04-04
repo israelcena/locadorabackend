71802de6c94708495439bc0260ab0d9d20f9855a
Israel Cena <israelsjm@gmail.com>
2022-04-04 13:49:02 -0300

    include findByCPF

--- src/main/java/com/israelcena/locadorabackend/cliente/repository/ClienteRepository.java
+++ src/main/java/com/israelcena/locadorabackend/cliente/repository/ClienteRepository.java
@@ -3,7 +3,9 @@ package com.israelcena.locadorabackend.cliente.repository;
 import com.israelcena.locadorabackend.cliente.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+  Optional<Cliente> findByCpf(String cpf);
 }
