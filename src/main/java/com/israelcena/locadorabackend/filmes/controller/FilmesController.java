53eddff6dd673663b105b1ce5c63d9abf6d89303
Israel Cena <israelsjm@gmail.com>
2022-04-05 17:16:23 -0300

    include find one filme

--- src/main/java/com/israelcena/locadorabackend/filmes/controller/FilmesController.java
+++ src/main/java/com/israelcena/locadorabackend/filmes/controller/FilmesController.java
@@ -2,10 +2,11 @@ package com.israelcena.locadorabackend.filmes.controller;
 
 import com.israelcena.locadorabackend.filmes.dto.FilmesDTO;
 import com.israelcena.locadorabackend.filmes.model.Filme;
-import com.israelcena.locadorabackend.filmes.service.ListAllFilmesService;
+import com.israelcena.locadorabackend.filmes.service.FindFilmesService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +16,21 @@ import java.util.stream.Collectors;
 @RestController @RequestMapping("/filmes") @RequiredArgsConstructor
 public class FilmesController {
 
-  private final ListAllFilmesService listAllFilmesService;
+  private final FindFilmesService findFilmesService;
   private final ModelMapper modelMapper;
 
   public FilmesDTO convertFilme(Filme filme){ return modelMapper.map(filme, FilmesDTO.class); }
 
   @GetMapping
   public List<FilmesDTO> listarTodosFilmes(){
-    List<Filme> filmes = listAllFilmesService.listarTodosFilmes();
+    List<Filme> filmes = findFilmesService.listarTodosFilmes();
     return filmes.stream().map(this::convertFilme).collect(Collectors.toList());
   }
 
+  @GetMapping(path = "{id}")
+  public Filme listarUmFilme(@PathVariable Integer id) throws Exception {
+    return findFilmesService.listarUmFilme(id)
+        .orElse(null);
+  }
 
 }
--- src/main/java/com/israelcena/locadorabackend/filmes/service/FindFilmesService.java
+++ src/main/java/com/israelcena/locadorabackend/filmes/service/FindFilmesService.java
@@ -6,13 +6,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service @RequiredArgsConstructor
-public class ListAllFilmesService {
+public class FindFilmesService {
 
   private final FilmesReposiitory filmesReposiitory;
 
   public List<Filme> listarTodosFilmes() {
     return filmesReposiitory.findAll();
+  }
+
+  public Optional<Filme> listarUmFilme(Integer id) {
+    Boolean exist = filmesReposiitory.existsById(id);
+    if (!exist) {
+      throw new IllegalStateException("O Filme Não Existe");
+    }
+    return filmesReposiitory.findById(id);
   }
 }
